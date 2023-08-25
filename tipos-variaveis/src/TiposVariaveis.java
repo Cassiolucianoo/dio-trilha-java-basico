84ec64b878af5421009ce05b899c354e976e6ab2
Cassio luciano da silva <cassiolucianodasilva@gmail.com>
2023-08-25 09:52:35 -0300

    iniciando com variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,18 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        //tipos primitivos
+
+        int idade = 38;
+        String nome = "Cassio luciano da silva";
+        float altura = 168;
+        double salario = 2000.00;
+
+
+        //valor final 
+
+        final String sobrenome = "cassio luciano da silva";
+
     }
 }
